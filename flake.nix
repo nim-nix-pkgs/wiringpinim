d7677e6f607633595a35437735448c15be142063
hugosenari <hugosenari@gmail.com>
2022-03-12 02:08:52 -0300

    chore: re index wiringpinim

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Wrapper that implements some of wiringPi's function for controlling a Raspberry Pi'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."wiringpinim-master".url = "path:./master";
+  inputs."wiringpinim-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wiringpinim-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
