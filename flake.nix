4c9f048bbad739e4756a9d50f30bf64db682e086
hugosenari <hugosenari@gmail.com>
2022-05-12 11:58:55 -0300

    chore: re index wiringpinim

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."wiringpinim-master".dir   = "master";
+  inputs."wiringpinim-master".owner = "nim-nix-pkgs";
+  inputs."wiringpinim-master".ref   = "master";
+  inputs."wiringpinim-master".repo  = "wiringpinim";
+  inputs."wiringpinim-master".type  = "github";
+  inputs."wiringpinim-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."wiringpinim-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
